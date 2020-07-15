55545e944f45bb4012a6d16cb28baec7e6f8f76c
Thomas Van <tinbolw@gmail.com>
2020-07-14 21:02:00 -0700

    added an purchasable upgrade

--- main.py
+++ main.py
@@ -16,12 +16,26 @@ def addToValue():
 	valueLabel.delete(0, tk.END)
 	valueLabel.insert(0, value)
 
-valueLabel = tk.Entry(root)
+def buyUpgrade():
+	global value
+	global valueAdd
+	if value >= 50:
+		value = value - 50
+		valueLabel.delete(0, tk.END)
+		valueLabel.insert(0, value)
+		valueAdd = valueAdd + 1
+	else:
+		valueLabel.delete(0, tk.END)
+		valueLabel.insert(0, "Not Enough Money")
 
+valueLabel = tk.Entry(root)
 valueLabel.pack()
 
 valueButton = tk.Button(root, width=15, height=5, text = "Button", command = addToValue)
 valueButton.pack()
+
+buyHelperButton = tk.Button(root, width=10, height=3, text="Buy Upgrade (50), command=buyUpgrade)
+buyHelperButton.pack()
 
 root.mainloop()
 
